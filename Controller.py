587e09895c0c3ca85472eb5ca8c11391c2a1cb44
svenschoonebeek <sschoonebeek@tudelft.nl>
2025-02-20 15:10:01 +0100

    .

--- Controller.py
+++ Controller.py
@@ -11,28 +11,24 @@ class Controller:
         self.B = np.eye(2, 2)
         self.alpha = 5
         self.Q = np.vstack(([self.alpha, 0], [0, self.alpha]))
-        #self.R = np.vstack(([5, 0], [0, 5]))
         self.R = np.eye(2, 2)
         self.P = np.vstack(([1.48, 0.06], [0.06, 1.56]))
-        self.Np = 30
+        self.Np = 3
         self.agent = None
         self.max_iter = 20
         self.bounds = [(-1, 1)] * 2 * self.Np
         self.state_constraint = [-5, 5]
-        #self.state_constraint = None
         self.penalty_weight = 10
 
     def plot(self):
         fig = plt.figure(figsize=(10, 6))
-        figs = fig.subfigures(1, 3)
-        ax2 = figs[1].add_subplot(1, 1, 1)
-        print(self.agent.u_k)
-        print(self.agent.x_seq)
+        figs = fig.subfigures(1, 2)
+        ax2 = figs[0].add_subplot(1, 1, 1)
         ax2.plot(range(self.max_iter), self.agent.u_k)
         ax2.set_title("Input over time")
         ax2.set_xlabel("Time")
         ax2.set_ylabel("Input")
-        ax3 = figs[2].add_subplot(1, 1, 1)
+        ax3 = figs[1].add_subplot(1, 1, 1)
         ax3.plot(range(self.max_iter), self.agent.x_seq[:-1])
         ax3.set_title("State over time")
         ax3.set_xlabel("Time")
